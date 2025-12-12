142e8bac10b0d18c3a22fbc8c9b2e84d4bd0c639
Zaali Mohamed <zaali.mohamed3002@gmail.com>
2025-12-12 11:37:56 +0100

    Add backend microservices structure with Spring Boot

---
+++ backend/absence-service/src/test/java/com/ensam/absenceservice/AbsenceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensam.absenceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsenceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ backend/justification-service/src/main/java/com/ensam/justificationservice/JustificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ensam.justificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustificationServiceApplication.class, args);
+	}
+
+}
